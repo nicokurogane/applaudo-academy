16b30f6240d8cdc5d67bdf137c4ea234fe8f91f6
Nicolas Martinez <fyedream@gmail.com>
2019-10-20 17:01:28 -0600

    Added section for former traineess for desktop

--- js/loadTraineesInfo.js
+++ js/loadTraineesInfo.js
@@ -5,6 +5,7 @@ document.onreadystatechange = function() {
     const containers = document.querySelectorAll(".trainee-container");
 
     containers.forEach((container, index) => {
+      if (index > 3) index -= 4;
       const { image, name, testimony } = traineeInfo[index];
       const imageNode = container.querySelector(".photo");
       const testimonyNode = container.querySelector(".testimony");
