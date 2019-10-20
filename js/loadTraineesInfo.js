aac3d9fb5cbac2ae7fb9cb0d53a68f8a8b28b82d
Nicolas Martinez <fyedream@gmail.com>
2019-10-20 16:07:20 -0600

    Added carousel for former trainees section

---
+++ js/loadTraineesInfo.js
@@ -0,0 +1,18 @@
+import traineeInfo from "./traineesInfo.js";
+
+document.onreadystatechange = function() {
+  if (document.readyState == "complete") {
+    const containers = document.querySelectorAll(".trainee-container");
+
+    containers.forEach((container, index) => {
+      const { image, name, testimony } = traineeInfo[index];
+      const imageNode = container.querySelector(".photo");
+      const testimonyNode = container.querySelector(".testimony");
+      const nameNode = container.querySelector(".name");
+
+      imageNode.src = image;
+      testimonyNode.innerHTML = testimony;
+      nameNode.innerHTML = name;
+    });
+  }
+};
---
+++ js/traineesInfo.js
@@ -0,0 +1,27 @@
+export default {
+  0: {
+    image: "assets/images/trainees/trainee1.jpg",
+    name: "Samuel Cornejo",
+    testimony: `"The Trainee Program gave me the opportunity to finally do what I like the most: programming. 
+      The program's content was organized in such a way I could gradually improve my programming skills. 
+      But, what helped me the most to improve faster was the selfless help from the team and their great teaching methods."`
+  },
+  1: {
+    image: "assets/images/trainees/trainee2.jpg",
+    name: "Gabriel Echeverria",
+    testimony: `"Being part of the Applaudo Trainee Program is one of the best opportunities I've received; 
+    it helped me challenge myself, meet new people, grow my career path and thriving to never stop improving everyday."`
+  },
+  2: {
+    image: "assets/images/trainees/trainee3.jpg",
+    name: "Rafael Jiménez",
+    testimony: `"When I was part of the Backend Trainee Program I learned about the best practices for developing web applications. 
+    I also learned about Agile methodologies that allow me to be a better coworker and I’m sure that it will help me improve on my professional career."`
+  },
+  3: {
+    image: "assets/images/trainees/trainee4.jpg",
+    name: "Jonathan Merlos",
+    testimony: `"Being a trainee challenged me to exercise my technical and leadership skills, it offered me a structured insight into the best Software Development practices in the industry.
+     I was surrounded by great professionals who willingly offered their support and made me commit to a process of collaboration and continuous self-improvement."`
+  }
+};
