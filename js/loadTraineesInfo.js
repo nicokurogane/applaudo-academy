ecfe5d1da0bc1a783fb94cc7209719c2a60f21ed
nicokurogane <fyedream@gmail.com>
2019-10-21 09:19:43 -0600

    fixed footer

--- js/loadTraineesInfo.js
+++ js/loadTraineesInfo.js
@@ -4,6 +4,11 @@ document.onreadystatechange = function() {
   if (document.readyState == "complete") {
     const containers = document.querySelectorAll(".trainee-container");
 
+    var browserWidth = Math.max(
+      document.documentElement.clientWidth,
+      window.innerWidth || 0
+    );
+    console.log(browserWidth);
     containers.forEach((container, index) => {
       if (index > 3) index -= 4;
       const { image, name, testimony } = traineeInfo[index];
